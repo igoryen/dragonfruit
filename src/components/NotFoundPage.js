6a38d43e1125e14cb64b5d2871eed631a4e4b10a
igoryen <igor.yentaltsev@gmail.com>
2020-09-06 14:13:30 -0400

    Added page 'not found' and Switch

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export class NotFoundPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>404!</h3>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './index.css';
 import SpanishVerbSearch from './components/SpanishVerbSearch'
 import * as serviceWorker from './serviceWorker';
 import { AddSpanishVerb } from './components/AddSpanishVerb';
 import { EditSpanishVerb } from './components/EditSpanishVerb';
+import { NotFoundPage } from './components/NotFoundPage';
 
 
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route path="/" component={SpanishVerbSearch} exact={true}/>
             <Route path="/add-verb" component={AddSpanishVerb} />
             <Route path="/edit-verb" component={EditSpanishVerb} />
-        </div>
+            <Route component={NotFoundPage} />
+        </Switch>
     </BrowserRouter>
 );
 
