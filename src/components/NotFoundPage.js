c989b45743709c05ab5d1c9c8cd297354cd112b1
igoryen <igor.yentaltsev@gmail.com>
2020-09-06 14:15:48 -0400

    Add link 'Go home' to the 404 page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export class NotFoundPage extends React.Component {
     render() {
         return (
             <div>
-                <h3>404!</h3>
+                <h3>404! <Link to="/">Go home</Link></h3>
             </div>
         )
     }
