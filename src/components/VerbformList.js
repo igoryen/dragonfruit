0b2b73d14af48da606718e53c8e6ea031a5948cb
igoryen <igor.yentaltsev@gmail.com>
2020-09-06 18:05:23 -0400

    Display filters

--- src/components/VerbformList.js
+++ src/components/VerbformList.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
-// import VerbformListItem from './VerbformListItem'
 
 const VerbformList = (props) => (
     <div>
         <h1>List of verb-forms</h1>
-        {props.verbforms.length}
+        {props.filters.text} | {props.verbforms.length}
     </div>
 )
 
 const mapStateToProps = (state) => {
     return {
-        verbforms: state.verbforms
+        verbforms: state.verbforms,
+        filters: state.filters
     }
 }
 
