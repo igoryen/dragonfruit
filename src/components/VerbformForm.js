b0bc9bb7e61446b979eb4f03e955c0d638ec6da4
igoryen <igor.yentaltsev@gmail.com>
2020-09-08 11:57:48 -0400

    Connected form input elements to state

--- src/components/VerbformForm.js
+++ src/components/VerbformForm.js
@@ -1,6 +1,21 @@
 import React from 'react'
 
 export default class VerbformForm extends React.Component {
+
+    state = {
+        spa: '',
+        rus: ''
+    }
+
+    onSpaChange = (e) => {
+        const spa = e.target.value
+        this.setState( () => ({ spa }) )
+    }
+    onRusChange = (e) => {
+        const rus = e.target.value
+        this.setState( () => ({ rus }) )
+    }
+
     render() {
         return (
             <div>
@@ -9,10 +24,14 @@ export default class VerbformForm extends React.Component {
                         type="text"
                         placeholder="Spanish verb-form"
                         autoFocus
+                        value={this.state.spa}
+                        onChange={this.onSpaChange}
                     />
                     <input
                         type="text"
                         placeholder="Russian verb-form"
+                        value={this.state.rus}
+                        onChange={this.onRusChange}
                     />
                     <button>Add verb-form</button>
                 </form>
