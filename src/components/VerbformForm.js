d8aca56a841fcc1a0f9124e807573e7add32f580
igoryen <igor.yentaltsev@gmail.com>
2020-09-08 11:51:28 -0400

    Added form elements to form 'Verb-form'

--- src/components/VerbformForm.js
+++ src/components/VerbformForm.js
@@ -3,7 +3,20 @@ import React from 'react'
 export default class VerbformForm extends React.Component {
     render() {
         return (
-            <div>Verbform form</div>
+            <div>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Spanish verb-form"
+                        autoFocus
+                    />
+                    <input
+                        type="text"
+                        placeholder="Russian verb-form"
+                    />
+                    <button>Add verb-form</button>
+                </form>
+            </div>
         )
     }
 }
