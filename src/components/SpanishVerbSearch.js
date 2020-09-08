71da421ef8b614dec61aa879db94592f365e6d54
igoryen <igor.yentaltsev@gmail.com>
2020-09-08 10:28:58 -0400

    Added comp 'verbform filters'

--- src/components/SpanishVerbSearch.js
+++ src/components/SpanishVerbSearch.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import VerbformList from './VerbformList';
+import VerbformListFilters from './VerbformListFilters';
 
 
 export class SpanishVerbSearch extends React.Component {
     render() {
         return (
             <div>
+                <VerbformListFilters />
                 <VerbformList />
             </div>
         )
---
+++ src/components/VerbformListFilters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VerbformListFilters = () => (
+    <div>
+        <input type="text" />
+    </div>
+)
+
+export default VerbformListFilters
