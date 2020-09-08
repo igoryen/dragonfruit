f8b74459d294fee61f91b92f22b84e545d506935
igoryen <igor.yentaltsev@gmail.com>
2020-09-08 18:54:56 -0400

    Limit the number of items displayed on dashboard

--- src/components/VerbformList.js
+++ src/components/VerbformList.js
@@ -7,7 +7,7 @@ const VerbformList = (props) => (
     <div>
         <h1>List of verb-forms</h1>
         {
-            props.verbforms.map( (verbform) => {
+            props.verbforms.slice(0,5).map( (verbform) => {
                 return <VerbformListItem key={verbform.id} {...verbform} />
             })
         }
