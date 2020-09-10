631db8cba43c58790f6b35b63a0d90ec0a8c82f1
igoryen <igor.yentaltsev@gmail.com>
2020-09-10 14:11:48 -0400

    Added pagination to view sorted items

--- src/components/VerbformList.js
+++ src/components/VerbformList.js
@@ -4,28 +4,57 @@ import VerbformListItem from './VerbformListItem'
 import selectVerbforms from '../selectors/verbforms'
 
 export class VerbformList extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            currentPage: 1,
+            todosPerPage: 10,
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
 
+    handleClick(event) {
+        this.setState({
+            currentPage: Number(event.target.id)
+        })
+    }
     render() {
+        const {
+            currentPage,
+            todosPerPage,
+        } = this.state
+
+        const pageNumbers = []
+        for (let i = 1; i <= Math.ceil(this.props.verbforms.length / 10); i++) {
+            pageNumbers.push(i)
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+                <li
+                    key={number}
+                    id={number}
+                    onClick={this.handleClick}
+                >
+                    {number}
+                </li>
+            )
+        })
+
         return (
             <div className="content-container">
-
                 <div className="list-header">
-                    <div className="show-for-mobile">SPA</div>
-                    <div className="show-for-mobile">RUS</div>
+                    <div className="show-for-mobile">RUS - SPA</div>
                 </div>
-                <div className="list-body">
+                <div className="list-body list-body__paginated">
                     {
-                        this.props.verbforms.length === 0 ? (
-                            <div className="list-item list-item--message">
-                                <span>No verb-forms</span>
-                            </div>
-                        )
-                        : (
-                            this.props.verbforms.slice(0, 15).map((verbform) => {
-                                return <VerbformListItem key={verbform.id} {...verbform} />
-                            })
-                        )
+                        this.props.verbforms.slice((currentPage * todosPerPage) - todosPerPage, (currentPage * todosPerPage)).map((verbform) => {
+                            return <VerbformListItem key={verbform.id} {...verbform} />
+                        })
                     }
+                    <ul id="page-numbers">
+                        {renderPageNumbers}
+                    </ul>
                 </div>
             </div>
         )
