f045c69b0726e3289bc6411e749e46a342d9d8c2
igoryen <igor.yentaltsev@gmail.com>
2020-09-10 14:11:48 -0400

    Converted verbformlist into a class component

--- src/components/VerbformList.js
+++ src/components/VerbformList.js
@@ -3,29 +3,34 @@ import { connect } from 'react-redux'
 import VerbformListItem from './VerbformListItem'
 import selectVerbforms from '../selectors/verbforms'
 
-const VerbformList = (props) => (
-    <div className="content-container">
-        <div className="list-header">
-            <div className="show-for-mobile">SPA</div>
-            <div className="show-for-mobile">RUS</div>
-        </div>
-        <div className="list-body">
-            {
-                props.verbforms.length === 0 ? (
-                    <div className="list-item list-item--message">
-                        <span>No verb-forms</span>
-                    </div>
-                )
-                : (
-                    props.verbforms.slice(0,15).map( (verbform) => {
-                        return <VerbformListItem key={verbform.id} {...verbform} />
-                    })
-                )
-            }
-        </div>
-    </div>
-)
+export class VerbformList extends React.Component {
 
+    render() {
+        return (
+            <div className="content-container">
+
+                <div className="list-header">
+                    <div className="show-for-mobile">SPA</div>
+                    <div className="show-for-mobile">RUS</div>
+                </div>
+                <div className="list-body">
+                    {
+                        this.props.verbforms.length === 0 ? (
+                            <div className="list-item list-item--message">
+                                <span>No verb-forms</span>
+                            </div>
+                        )
+                        : (
+                            this.props.verbforms.slice(0, 15).map((verbform) => {
+                                return <VerbformListItem key={verbform.id} {...verbform} />
+                            })
+                        )
+                    }
+                </div>
+            </div>
+        )
+    }
+}
 const mapStateToProps = (state) => {
     return {
         verbforms: selectVerbforms(state.verbforms, state.filters)
