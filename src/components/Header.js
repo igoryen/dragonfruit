08e204e0a500e64641692e5fff3bfb27bd5df35b
igoryen <igor.yentaltsev@gmail.com>
2020-09-10 14:11:48 -0400

    Header colors.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { startLogout } from '../actions/auth'
 
 
 export const Header = ({ startLogout }) => (
-    <header>
-        <h1>Spanish verb forms</h1>
-        <NavLink activeClassName="is-active" to="/dashboard" >Dashboard</NavLink>
-        <NavLink activeClassName="is-active" to="/add-verb" >Add verb Form</NavLink>
-        <NavLink activeClassName="is-active" to="/edit-verb" >Edit verb Form</NavLink>
+    <header className="header">
+        <Link className="header__title" to="/dashboard">Dragonfruit</Link>
         <button onClick={startLogout}>Logout</button>
     </header>
 )
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,7 +5,7 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
-            <h1 className="box-layout__title">Dragon Fruit</h1>
+            <h1 className="box-layout__title">Dragonfruit</h1>
             <button className="button" onClick={startLogin}>Login</button>
         </div>
     </div>
