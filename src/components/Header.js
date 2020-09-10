3361b26d1595ed2efd2d8cf88601d08591809f5b
igoryen <igor.yentaltsev@gmail.com>
2020-09-10 14:11:48 -0400

    Styling Summary Area.

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ export const Header = ({ startLogout }) => (
         <div className="content-container">
             <div className="header__content">
                 <Link className="header__title" to="/dashboard">Dragonfruit</Link>
-                <button onClick={startLogout}>Logout</button>
+                <button className="button button--link" onClick={startLogout}>Logout</button>
             </div>
         </div>
     </header>
--- src/components/VerbformsSummary.js
+++ src/components/VerbformsSummary.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import selectVerbforms from '../selectors/verbforms'
 import selectVerbformsTotal from '../selectors/verbforms-total'
 
@@ -7,8 +8,13 @@ export const VerbformsSummary = ({ verbformCount, verbformsTotal }) => {
     const verbformWord = verbformCount === 1 ? 'verbform' : 'verbforms'
 
     return (
-        <div>
-            <h1>Viewing {verbformCount} {verbformWord}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{verbformCount}</span> {verbformWord}</h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/add-verb" autoFocus>Add Verb</Link>
+                </div>
+            </div>
         </div>
     )
 }
