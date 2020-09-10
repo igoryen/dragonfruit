302951f5e75fb355325007b0cd13055b2e5e1151
igoryen <igor.yentaltsev@gmail.com>
2020-09-10 14:11:48 -0400

    Styling List Filters.

--- src/components/VerbformListFilters.js
+++ src/components/VerbformListFilters.js
@@ -3,31 +3,37 @@ import { connect } from 'react-redux'
 import { setTextFilter, sortByRussian, sortBySpanish, sortByCreatedAt } from '../actions/filters'
 
 const VerbformListFilters = (props) => (
-    <div>
-        <input
-            type="text"
-            value={props.filters.text}
-            onChange={ (e) => {
-                props.dispatch( setTextFilter(e.target.value) )
-            }}
-        />
-        <select
-            value={props.filters.sortBy}
-            onChange={ (e) => {
-                if (e.target.value === 'rus') {
-                    props.dispatch( sortByRussian() )
-                } else if (e.target.value === 'spa') {
-                    props.dispatch( sortBySpanish() )
-                }
-                else if (e.target.value === 'createdAt') {
-                    props.dispatch( sortByCreatedAt() )
-                }
-            }}
-        >
-            <option value="createdAt">createdAt</option>
-            <option value="rus">Russian</option>
-            <option value="spa">Spanish</option>
-        </select>
+    <div className="content-container">
+        <div className="input-group">
+            <div className="input-group__item">
+                <input
+                    type="text"
+                    value={props.filters.text}
+                    onChange={ (e) => {
+                        props.dispatch( setTextFilter(e.target.value) )
+                    }}
+                />
+            </div>
+            <div className="input-group__item">
+                <select
+                    value={props.filters.sortBy}
+                    onChange={ (e) => {
+                        if (e.target.value === 'rus') {
+                            props.dispatch( sortByRussian() )
+                        } else if (e.target.value === 'spa') {
+                            props.dispatch( sortBySpanish() )
+                        }
+                        else if (e.target.value === 'createdAt') {
+                            props.dispatch( sortByCreatedAt() )
+                        }
+                    }}
+                >
+                    <option value="createdAt">createdAt</option>
+                    <option value="rus">Russian</option>
+                    <option value="spa">Spanish</option>
+                </select>
+            </div>
+        </div>
     </div>
 )
 
